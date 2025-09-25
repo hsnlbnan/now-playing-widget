6c59bf05cbbe307a806a7e2f56932eb3df113ad1
Hüsnü Lübnan <55919762+hsnlbnan@users.noreply.github.com>
2025-09-25 20:40:52 +0300

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
         <div className="pointer-events-none fixed inset-0 -z-10 [background:radial-gradient(1200px_600px_at_50%_-10%,#1DB95420,transparent_70%),radial-gradient(900px_600px_at_80%_20%,#60A5FA26,transparent_65%),radial-gradient(800px_500px_at_20%_30%,#A78BFA1f,transparent_60%)]" />
         <div className="pointer-events-none fixed inset-0 -z-10 backdrop-blur-[2px]" />
         <main className="px-4 sm:px-6 md:px-8 py-8">{children}</main>
+        <Analytics />
       </body>
     </html>
   );
